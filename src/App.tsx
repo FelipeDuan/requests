7e85f83463261080b0dbc1c35d31fda3682903c2
FelipeDuan <felipeduan1@gmail.com>
2025-07-06 19:50:46 -0300

    feat: testando a Tanstack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UserInfo } from "./components/user-info";
 import { UserNewForm } from "./components/user-new-form";
+import { UsersList } from "./components/users-list";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <UserInfo />
-      <br />
-      <UserNewForm />
+      <QueryClientProvider client={queryClient}>
+        <UserInfo />
+        <br />
+        <UserNewForm />
+        <br />
+        <UsersList />
+      </QueryClientProvider>
     </>
   );
 }
