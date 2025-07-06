9887f1697f401c3582600c616956dcfa797aba45
FelipeDuan <felipeduan1@gmail.com>
2025-07-06 19:32:59 -0300

    feat: form de cadastro de users

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { UserInfo } from "./components/user-info";
+import { UserNewForm } from "./components/user-new-form";
 
 function App() {
   return (
     <>
       <UserInfo />
+      <br />
+      <UserNewForm />
     </>
   );
 }
